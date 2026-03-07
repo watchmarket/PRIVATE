965a0f2d0769913f405e70d8ba2cb18e67d7900a
watchmarket <94284201+watchmarket@users.noreply.github.com>
2026-03-07 14:04:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const CONFIG_APP = {
         //  NAME: "APP_PANTAUAN",
         //NAME: "WATCHMARKET-TEST",
         NAME: "APP PRIVATE",
-        VERSION: "2026.03.06",
+        VERSION: "2026.03.07",
         SCAN_LIMIT: false,
         AUTORUN: true,
         AUTO_VOLUME: true,  // cek volume otomatis untuk filter dan alert
